7201c70359b7a24d18b7dd5d78fab475362aa6e7
nwilliamfeng <nwilliamfeng@163.com>
2017-03-06 23:02:09 +0800

    添加项目文件。

---
+++ MEAS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MEAS.MvcApplication" Language="C#" %>
---
+++ MEAS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MEAS.Startup))]
+namespace MEAS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
