1fc139a1ee018948af7665a9128c6db74e3331de
fw <fw@DESKTOP-07EN549>
2017-04-12 20:12:33 +0800

    update412b

--- MEAS/Controllers/ProductController.cs
+++ MEAS/Controllers/ProductController.cs
@@ -26,7 +26,8 @@ namespace MEAS.Controllers
 
         public async Task<ActionResult> List(string category,int page=1 )
         {
-          
+            if (category == null)
+                return this.RedirectToAction("Index", "Home");
             var results = await this._service.FindWithCategory(category);
             var vm = new PagingResult<Product> { Values = results.Skip(PageSize * (page - 1)).Take(PageSize), CurrentPage = page, ItemsPerPage = PageSize, TotalItems = results.Count() };
         
