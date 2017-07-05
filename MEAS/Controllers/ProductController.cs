788425c5a796172fc43bbbb87d2d19ac01b9fd96
fw <fw@DESKTOP-07EN549>
2017-07-05 18:04:19 +0800

    update 170705

--- MEAS/Controllers/ProductController.cs
+++ MEAS/Controllers/ProductController.cs
@@ -19,21 +19,25 @@ namespace MEAS.Controllers
             this._service = service;
         }
 
+      
         public async Task<ActionResult> Index()
         {
+            this.TempData["key"] = "111";
             var category = "Chess";
+            
             return this.RedirectToAction("List", new {  category }); 
-           // return View();
         }
 
         public async Task<ActionResult> List(string category,int page=1 )
         {
+     
             if (category == null)
                 return this.RedirectToAction("Index", "Home");
-
+            var value =TempData["key"];
+            Console.WriteLine(value);
             var results = await this._service.FindWithCategory(category);
             var vm = new PagingResult<Product> { Values = results.Skip(PageSize * (page - 1)).Take(PageSize), CurrentPage = page, ItemsPerPage = PageSize, TotalItems = results.Count() };
-        
+         
           
             return View(vm);
         }
